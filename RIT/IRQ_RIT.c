4627e5ba025a8aa079598daf876b9850a44d17a6
Ema <emanuelecoricciati@libero.it>
2023-12-26 12:49:46 +0100

    project init

---
+++ RIT/IRQ_RIT.c
@@ -0,0 +1,55 @@
+/*********************************************************************************************************
+**--------------File Info---------------------------------------------------------------------------------
+** File name:           IRQ_RIT.c
+** Last modified Date:  2014-09-25
+** Last Version:        V1.00
+** Descriptions:        functions to manage T0 and T1 interrupts
+** Correlated files:    RIT.h
+**--------------------------------------------------------------------------------------------------------
+*********************************************************************************************************/
+#include "lpc17xx.h"
+#include "RIT.h"
+#include "../GLCD/GLCD.h"
+
+/******************************************************************************
+** Function name:		RIT_IRQHandler
+**
+** Descriptions:		REPETITIVE INTERRUPT TIMER handler
+**
+** parameters:			None
+** Returned value:		None
+**
+******************************************************************************/
+
+void RIT_IRQHandler (void)
+{			
+	static int down=0;	
+	down++;
+	if((LPC_GPIO2->FIOPIN & (1<<11)) == 0){
+		LCD_Clear(White);
+		reset_RIT();
+		switch(down){
+			case 1:
+				display_grid();
+				break;
+			default:
+				break;
+		}
+	}
+	else {	/* button released */
+		down=0;			
+		disable_RIT();
+		reset_RIT();
+		NVIC_EnableIRQ(EINT1_IRQn);							 /* disable Button interrupts			*/
+		LPC_PINCON->PINSEL4    |= (1 << 22);     /* External interrupt 0 pin selection */
+	}
+		
+	
+  LPC_RIT->RICTRL |= 0x1;	/* clear interrupt flag */
+	
+  return;
+}
+
+/******************************************************************************
+**                            End Of File
+******************************************************************************/
